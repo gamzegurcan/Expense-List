25236130d850a1b5cfd1c7071ea19d6f2479bf98
Gamze Gürcan <gmzgrcn18812@icloud.com>
2021-12-21 13:33:53 +0300

    building a first custom component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,20 @@
-function ExpenseItem () {
-  return <h2>Expense Item</h2>;
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 11, 21);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
+  return (
+    <div className='expense-item' >
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description' >
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
+
