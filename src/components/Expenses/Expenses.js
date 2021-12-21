f3320ad3314c59200b63320feaf5ce7780a205b8
Gamze Gürcan <gmzgrcn18812@icloud.com>
2021-12-21 16:41:01 +0300

    organized component files

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,12 @@
+import React from 'react';
+
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import './Expenses.css';
 
 function Expenses(props){
 return(
-  <div className="expenses">
+  <Card className="expenses">
     <ExpenseItem 
         title= {props.items[0].title} 
         amount={props.items[0].amount} 
@@ -23,7 +27,7 @@ return(
         amount={props.items[3].amount} 
         date= {props.items[3].date} 
       ></ExpenseItem>
-  </div>
+  </Card>
 
 );
 }
